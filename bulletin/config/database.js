f921114e212fc5d21435769170e2f6c26690f4df
YoungJun <kyjun92@gmail.com>
2021-01-12 13:37:16 +0900

    work 01

---
+++ bulletin/config/database.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var db_info = {
+    host: 'localhost',
+    port: '3366',
+    user: 'root',
+    password: '1234',
+    database: 'shop'
+}
+
+module.exports = {
+    init: function () {
+        return mysql.createConnection(db_info);
+    },
+    connect: function(conn) {
+        conn.connect(function(err) {
+            if(err) console.error('mysql connection error : ' + err);
+            else console.log('mysql is connected successfully!');
+        });
+    }
+}
---
+++ syntax/Variable.js
@@ -0,0 +1,3 @@
+var a = 1;
+var b = 13;
+console.log(a+b);
